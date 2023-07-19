454ea38b63d69f92b09387bb3bc9862af415ca2c
MishynaIra <gortenzia1987@gmail.com>
2023-07-19 19:33:14 +0300

    11.прописала с помощью тернарника у li условия отображения активной категории

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,18 @@
+import {useState} from "react";
+
 function Categories() {
+
+    const [activeCategorie, setActiveCategorie] = useState(1);
+
     return(
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li className={activeCategorie ===0 ? 'active' : ''}>Все</li>
+                <li className={activeCategorie ===1 ? 'active' : ''}>Мясные</li>
+                <li className={activeCategorie ===2 ? 'active' : ''}>Вегетарианская</li>
+                <li className={activeCategorie ===3 ? 'active' : ''}>Гриль</li>
+                <li className={activeCategorie ===4 ? 'active' : ''}>Острые</li>
+                <li className={activeCategorie ===5 ? 'active' : ''}>Закрытые</li>
             </ul>
         </div>
     )
