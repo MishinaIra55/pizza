309199ebca220641776efab807cd62af13408d18
MishynaIra <gortenzia1987@gmail.com>
2023-07-19 19:44:52 +0300

    12.повешала onClick на каждую li чтобы выбирать необходимую категорию .при клике решаем какой индекс передаем.

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,15 +4,19 @@ function Categories() {
 
     const [activeCategorie, setActiveCategorie] = useState(1);
 
+    const onClickCategory = (index) => {
+        setActiveCategorie(index);
+    }
+
     return(
         <div className="categories">
             <ul>
-                <li className={activeCategorie ===0 ? 'active' : ''}>Все</li>
-                <li className={activeCategorie ===1 ? 'active' : ''}>Мясные</li>
-                <li className={activeCategorie ===2 ? 'active' : ''}>Вегетарианская</li>
-                <li className={activeCategorie ===3 ? 'active' : ''}>Гриль</li>
-                <li className={activeCategorie ===4 ? 'active' : ''}>Острые</li>
-                <li className={activeCategorie ===5 ? 'active' : ''}>Закрытые</li>
+                <li onClick={()=> onClickCategory(0)} className={activeCategorie ===0 ? 'active' : ''}>Все</li>
+                <li onClick={()=> onClickCategory(1)} className={activeCategorie ===1 ? 'active' : ''}>Мясные</li>
+                <li onClick={()=> onClickCategory(2)} className={activeCategorie ===2 ? 'active' : ''}>Вегетарианская</li>
+                <li onClick={()=> onClickCategory(3)} className={activeCategorie ===3 ? 'active' : ''}>Гриль</li>
+                <li onClick={()=> onClickCategory(4)} className={activeCategorie ===4 ? 'active' : ''}>Острые</li>
+                <li onClick={()=> onClickCategory(5)} className={activeCategorie ===5 ? 'active' : ''}>Закрытые</li>
             </ul>
         </div>
     )
