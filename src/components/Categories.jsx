d2bc4afd169dede72694c647ea4d8eef3460ab7b
MishynaIra <gortenzia1987@gmail.com>
2023-07-20 10:52:52 +0300

    13.создала массив категории и сделала рендеринг списка map/index

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,9 @@ import {useState} from "react";
 
 function Categories() {
 
-    const [activeCategorie, setActiveCategorie] = useState(1);
+    const [activeCategorie, setActiveCategorie] = useState(0);
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская','Гриль', 'Острые', 'Закрытые']
 
     const onClickCategory = (index) => {
         setActiveCategorie(index);
@@ -11,12 +13,10 @@ function Categories() {
     return(
         <div className="categories">
             <ul>
-                <li onClick={()=> onClickCategory(0)} className={activeCategorie ===0 ? 'active' : ''}>Все</li>
-                <li onClick={()=> onClickCategory(1)} className={activeCategorie ===1 ? 'active' : ''}>Мясные</li>
-                <li onClick={()=> onClickCategory(2)} className={activeCategorie ===2 ? 'active' : ''}>Вегетарианская</li>
-                <li onClick={()=> onClickCategory(3)} className={activeCategorie ===3 ? 'active' : ''}>Гриль</li>
-                <li onClick={()=> onClickCategory(4)} className={activeCategorie ===4 ? 'active' : ''}>Острые</li>
-                <li onClick={()=> onClickCategory(5)} className={activeCategorie ===5 ? 'active' : ''}>Закрытые</li>
+                {categories.map((item, index) => (
+                    <li onClick={()=> onClickCategory(index)} className={activeCategorie === index ? 'active' : ''}>{item}</li>
+                ))}
+
             </ul>
         </div>
     )
