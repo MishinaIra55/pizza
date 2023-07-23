0eeff12eea03dedb9aaff8e4c2327ad0ae1d850f
MishynaIra <gortenzia1987@gmail.com>
2023-07-23 12:01:42 +0300

    31.создала state для загрузки skeletona/изначально пиццы загружаются

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Skeleton from "./components/PizzaBlock/Skeleton";
 
 function App() {
     const [pizzas, setPizzas] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://64ba32de5e0670a501d5cb15.mockapi.io/items')
@@ -34,7 +35,7 @@ function App() {
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
                         {pizzas.map((item) => (
-                            <Skeleton
+                            <PizzaBlock
                                 key={item.id}
                                 title={item.title}
                                 price={item.price} i
