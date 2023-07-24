270129754247aa5252363473849655409b88c87f
MishynaIra <gortenzia1987@gmail.com>
2023-07-24 12:22:42 +0300

    route для корцины. по нажатию на кнопку стоимости пиццы переходим на страницу корзины

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './scss/app.scss';
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Cart from './pages/Cart';
+
 import {Route, Routes} from "react-router-dom";
 
 
@@ -13,6 +15,7 @@ function App() {
                 <div className="container">
                    <Routes>
                        <Route path='/' element={<Home/>}/>
+                       <Route path='/cart' element={<Cart/>}/>
                        <Route path='*' element={<NotFound/>}/>
                    </Routes>
                 </div>
