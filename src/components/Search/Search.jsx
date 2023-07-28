2864b3b40035cee276e5f9dbc9700abdef163733
MishynaIra <gortenzia1987@gmail.com>
2023-07-28 10:46:21 +0300

    Разрабатываем пагинацию и поиск пицц  svg подключила как реакт компонент и стилизовала внитри inputa

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,15 @@
 import styles from './Search.module.scss';
+import { ReactComponent as MySVGIcon } from './MySVG.svg';
 
 const Search = () => {
     return (
-        <input className={styles.root} placeholder="Search pizza..."/>
+        <div className={styles.container}>
+            <input className={styles.root} placeholder="Search pizza..." />
+            <div className={styles.svgContainer}>
+                <MySVGIcon className={styles.svgIcon} />
+            </div>
+        </div>
+
     )
 }
 
