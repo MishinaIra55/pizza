1cb42c6ae51eda133e6cf0313548e835f9ae6261
MishynaIra <gortenzia1987@gmail.com>
2023-07-31 10:56:11 +0300

    Что такое контекст в React (useContext) и Props Drilling?  в компоненте search использую контекст из переменной SearchContext

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,11 @@
 import styles from './Search.module.scss';
 import { ReactComponent as MySVGIcon } from './MySVG.svg';
+import {useContext} from "react";
+import {SearchContext} from "../../App";
 
-const Search = ({searchValue, setSearchValue}) => {
+const Search = () => {
+    const {searchValue, setSearchValue} = useContext(SearchContext);
+
     return (
         <div className={styles.container}>
             <input
