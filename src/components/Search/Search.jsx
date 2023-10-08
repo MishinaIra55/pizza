8bade3431a612069c44756044b8bd0cc23237e56
MishynaIra <gortenzia1987@gmail.com>
2023-10-08 18:50:49 +0200

    28,focus on input

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,26 @@
 import styles from './Search.module.scss';
 import { ReactComponent as MySVGIcon } from './MySVG.svg';
-import {useContext} from "react";
+import {useContext, useRef} from "react";
 import {SearchContext} from "../../App";
 
 const Search = () => {
     const {searchValue, setSearchValue} = useContext(SearchContext);
+    const inputRef = useRef();
+
+    const onClickClear = () => {
+        setSearchValue('');
+        inputRef.current.focus();
+    }
 
     return (
         <div className={styles.container}>
             <input
+                ref={inputRef}
                 value={searchValue}
                 onChange={(event) => setSearchValue(event.target.value)}
                 className={styles.root}
                 placeholder="Search pizza..."
-                onClick={()=> setSearchValue('')}
+                onClick={onClickClear}
             />
             <div className={styles.svgContainer}>
                 <MySVGIcon className={styles.svgIcon} />
