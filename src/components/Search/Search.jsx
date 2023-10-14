a858e71d46cb2d73f4c80a8b80c41ea133e20027
MishynaIra <gortenzia1987@gmail.com>
2023-10-14 10:02:38 +0200

    29,debounce выполняем отложенно вызов функции поиска пиццы и количество запросов сократилось

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,23 +1,40 @@
 import styles from './Search.module.scss';
 import { ReactComponent as MySVGIcon } from './MySVG.svg';
-import {useContext, useRef} from "react";
+import {useCallback, useContext, useRef, useState} from "react";
 import {SearchContext} from "../../App";
+import debounce from 'lodash.debounce';
 
 const Search = () => {
-    const {searchValue, setSearchValue} = useContext(SearchContext);
+    const [value, setValue] = useState(''); //локально создала state + контролируемый input
+    const { setSearchValue} = useContext(SearchContext);
     const inputRef = useRef();
 
     const onClickClear = () => {
         setSearchValue('');
+        setValue('');
         inputRef.current.focus();
-    }
+    };
+
+    const updateSearchValue = useCallback(
+        debounce((str) => {
+            setSearchValue(str);
+        }, 1000),
+        []
+    );
+
+    const onChangeInput = (event) => {
+        setValue(event.target.value);
+        updateSearchValue(event.target.value);
+    } ;
+
+
 
     return (
         <div className={styles.container}>
             <input
                 ref={inputRef}
-                value={searchValue}
-                onChange={(event) => setSearchValue(event.target.value)}
+                value={value}
+                onChange={onChangeInput }
                 className={styles.root}
                 placeholder="Search pizza..."
                 onClick={onClickClear}
