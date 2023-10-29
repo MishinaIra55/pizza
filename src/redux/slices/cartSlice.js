ab2664cd372c92cf1ecfc7177527c350cdbd54b8
MishynaIra <gortenzia1987@gmail.com>
2023-10-29 11:31:51 +0100

    37.делаю корзину с помощью,делаю так чтобы пиццы не дублировались при добавлении,но count увеличивается на количество добаления пицц

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -9,11 +9,26 @@ const cartSlice = createSlice({
     name: ' cart',
     initialState,
     reducers: {
+        // addItem(state, action) {
+        //     state.items.push(action.payload);
+        //     state.totalPrice = state.items.reduce((amount, object) => {
+        //         return object.price + amount;
+        //     }, 0);
+        // },
         addItem(state, action) {
-            state.items.push(action.payload);
+            const findItem = state.items.find((object) => object.id !== action.payload)
+
+            if (findItem) {
+                findItem.count ++;
+            } else {
+                state.items.push({
+                    ...action.payload,
+                    count: 1,
+                });
+            }
             state.totalPrice = state.items.reduce((amount, object) => {
                 return object.price + amount;
-            }, 0);
+                }, 0);
         },
        removeItem(state, action) {
            state.items = state.items.filter(object => object.id !== action.payload);
