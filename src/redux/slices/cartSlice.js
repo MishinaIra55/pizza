769fd46b82e074706ab94109a054f246a27ec5a0
MishynaIra <gortenzia1987@gmail.com>
2023-10-29 10:30:51 +0100

    35,делаю корзину с помощью redux.добавление пиццы, удаление , очистка

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,24 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    totalPrice: 0,
+    items: []
+};
+
+const cartSlice = createSlice({
+    name: ' cart',
+    initialState,
+    reducers: {
+        addItem(state, action) {
+            state.items.push(action.payload);
+        },
+       removeItem(state, action) {
+           state.items = state.items.filter(object => object.id !== action.payload);
+        },
+        clearItems(state, action) {
+            state.items = [];
+        }
+    },
+});
+
+export const { addProduct } = cartSlice;
