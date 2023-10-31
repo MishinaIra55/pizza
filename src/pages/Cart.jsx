3f9251a3d4a204bc683d9ab1074de847216865b6
MishynaIra <gortenzia1987@gmail.com>
2023-10-31 18:00:54 +0100

    44.условный рендер для отображения пустой корзины при условии что totalCount =0

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import {Link} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import CartItem from "../components/CartItem";
 import {clearItems} from "../redux/slices/cartSlice";
+import CartEmpty from "../components/CartEmpty";
 
 const Cart = () => {
     const dispatch = useDispatch();
@@ -15,6 +16,10 @@ const Cart = () => {
             dispatch(clearItems());
         }
     };
+
+    if (!totalCount) {
+        return <CartEmpty />
+    }
 
     return (
 
