bc7c2fe00f0283d37a88ceec87018fb6b475c344
MishynaIra <gortenzia1987@gmail.com>
2023-11-05 19:25:53 +0100

    63,создала useEffect где создала функциб fetchPizza которая шлет запрос на бэк чтобы получить информацию о пицце

--- src/pages/FullPizza.js
+++ src/pages/FullPizza.js
@@ -1,7 +1,23 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export const FullPizza = () => {
     const { id } = useParams();
+    const [pizza, setPizza] = useState();
+
+    useEffect(() => {
+        async function fetchPizza() {
+            try {
+                const {data} = await axios.get('https://651e831944a3a8aa47687f71.mockapi.io/items/' + id);
+                setPizza(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchPizza();
+    }, []);
+
 
     return (
         <div className='container'>
