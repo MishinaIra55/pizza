4fb555184b6b322056acc31add03c23745719ee9
MishynaIra <gortenzia1987@gmail.com>
2023-11-05 18:55:24 +0100

    61.создала компонент FullPizza для отображения информации про пиццу на которую нажимаешь. + отдельный роутер

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Cart from './pages/Cart';
 import {Route, Routes} from "react-router-dom";
+import FullPizza from "./pages/FullPizza";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
                        <Route path='/' element={<Home />}/>
                        <Route path='/cart' element={<Cart/>}/>
                        <Route path='*' element={<NotFound/>}/>
+                       <Route path='/pizza/:id' element={<FullPizza/>}/>
                    </Routes>
                </div>
         </div>
---
+++ src/pages/FullPizza.js
@@ -0,0 +1,11 @@
+export const FullPizza = () => {
+    return (
+        <div>
+            <img src=''/>
+            <h2> 33333</h2>
+            <p>Інформація про піццу</p>
+        </div>
+    )
+};
+
+export default FullPizza;
