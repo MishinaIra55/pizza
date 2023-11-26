e8284d8c023f814d49bfa1b8831f2ecebe061068
MishynaIra <gortenzia1987@gmail.com>
2023-11-26 09:24:37 +0100

    typescript Categories яв-ся функциональным компонентом React.FC и функциональный комопнент хранит в себе <CategoriesProps>

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,6 +1,11 @@
+import React from "react";
 
+type CategoriesProps = {
+    value: number,
+    onClickCategory: any,
+};
 
-const Categories = ({value, onClickCategory}) => {
+const Categories: React.FC<CategoriesProps> = ({value, onClickCategory}) => {
 
     const categories = ['Все', 'Мясные', 'Вегетарианская','Гриль', 'Острые', 'Закрытые']
 
